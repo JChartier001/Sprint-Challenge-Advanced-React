3d22feba17268dfd2c0953443d52017705435358
JChartier001 <JChartier001@gmail.com>
2019-11-01 13:00:22 -0500

    Trying to test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,21 @@ import React from 'react';
 import {render} from "@testing-library/react";
 import App from './App';
 
+
 test('renders without crashing', () => {
-    render(<App />);
+  const {getAllByText} = render(<App />);
+  getAllByText(/strike/i)
+  
+});
+
+test('renders without crashing', () => {
+  const {getAllByText} = render(<App />);
+  getAllByText(/ball/i)
+  
+});
+
+test('renders without crashing', () => {
+  const {getByText} = render(<App />);
+  getByText(/foul/i)
   
 });

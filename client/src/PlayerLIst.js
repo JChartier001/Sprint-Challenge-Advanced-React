6d6ce04f9c07ee1e4d0c58b33f639eaf1229d60c
JChartier001 <JChartier001@gmail.com>
2019-11-01 10:12:14 -0500

    created playerlist.js

---
+++ client/src/PlayerLIst.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class PlayerList extends React.Component {
+    constructor(){
+        super();
+        this.state={
+            player: ''
+        }
+    }
+
+        componentDidMount(){
+
+        }
+
+    render(){
+        return(
+
+        );
+    };
+}
